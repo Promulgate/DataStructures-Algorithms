d55f04d528be3cfa55d84b97149b0be7b73f71aa
Eric Chang <echang25@hotmail.com>
2017-04-12 18:10:20 -0400

    prime factors

---
+++ PrimeFactors.swift
@@ -0,0 +1,32 @@
+//
+//  PrimeFactors.swift
+//  
+//
+//  Created by Eric Chang on 4/12/17.
+//
+//
+
+import Foundation
+
+func findPrimeFactors(_ number: Int) -> [Int] {
+    var num = number
+    var primes = [Int]()
+    var div = 2
+    
+    while div * div <= num {
+        while num % div == 0 {
+            primes.append(div)
+            num /= div
+        }
+        div += 1
+    }
+    
+    if num > 1 {
+        primes.append(num)
+    }
+    return primes
+}
+
+print(findPrimeFactors(64))
+print(findPrimeFactors(999))
+print(findPrimeFactors(5436789076598767))
